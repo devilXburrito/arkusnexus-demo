0d1f445d360bec4cb03e25071d163bd357d1863b
Francisco Garza <e-fhgarza@neoris.com>
2020-05-22 12:52:10 -0500

    introducing echarts on the project, adding redirect, fix circular import

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 import { PrivateRoutingModule } from './private-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
+// @echarts
+import * as echarts from 'echarts';
+import { NgxEchartsModule } from 'ngx-echarts';
+
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
-
-
 
 @NgModule({
   declarations: [UserDashboardComponent],
   imports: [
     PrivateRoutingModule,
+    NgxEchartsModule.forRoot({
+      echarts
+    }),
     SharedModule
   ]
 })
